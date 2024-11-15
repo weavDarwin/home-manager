584957b524d4cb915fb895576e28227a07c84d9f
Darwin Corn <darwin@weaviate.io>
2024-11-15 14:41:38 -0800

    darwin: make font copying optional
    Adds a target.darwin.copyFonts option for font copying, defaults to
    pkgs.stdenv.hostPlatform.isDarwin to preserve current behavior.

--- modules/targets/darwin/fonts.nix
+++ modules/targets/darwin/fonts.nix
@@ -11,9 +11,17 @@ let
   };
   fonts = "${fontsEnv}/share/fonts";
   installDir = "${homeDir}/Library/Fonts/HomeManager";
+  cfg = config.targets.darwin;
 in {
+  options.targets.darwin.copyFonts = mkOption {
+    type = with types; bool;
+    default = true;
+    description = ''
+      This will copy home-manager-installed fonts to $HOME/Library/Fonts/HomeManager to ensure their use by installed applications. MacOS won't recognize fonts merely symlinked there.
+    '';
+  };
   # macOS won't recognize symlinked fonts
-  config = mkIf pkgs.stdenv.hostPlatform.isDarwin {
+  config = mkIf cfg.copyFonts {
     home.file."Library/Fonts/.home-manager-fonts-version" = {
       text = "${fontsEnv}";
       onChange = ''
